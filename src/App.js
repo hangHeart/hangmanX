38a22f7fbba44b11dd4f218cc13005531a8117aa
adamgoren <adamjgoren@gmail.com>
2019-10-12 11:47:45 -0400

    build react wrapper component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 import io from 'socket.io-client';
 import "./App.css";
+import LetterWrapper from './letterWrapper'
 
 const socket = io.connect("http://localhost:4000");
 // https://codeburst.io/isomorphic-web-app-react-js-express-socket-io-e2f03a469cd3
@@ -12,18 +13,54 @@ class App extends Component{
     super(props);
     this.state = {
       color: 'red',
+      letters: {
+        'a': false,
+        'b': false,
+        'c': false,
+        'd': false,
+        'e': false,
+        'f': false,
+        'g': false,
+        'h': false,
+        'i': false,
+        'j': false,
+        'k': false,
+        'l': false,
+        'm': false,
+        'n': false,
+        'o': false,
+        'p': false,
+        'q': false,
+        'r': false,
+        's': false,
+        't': false,
+        'o': false,
+        'p': false,
+        'q': false,
+        'r': false,
+        's': false,
+        't': false,
+        'u': false,
+        'v': false,
+        'w': false,
+        'x': false,
+        'y': false,
+        'z': false,
+      },
+      answer: 'apple',
+      disp: '_____'
+      }
+      this.onClick = this.onClick.bind(this);
     }
-    this.onClick = this.onClick.bind(this);
-  }
 
   componentDidMount() {
     socket.on('connect', function () {
-      console.log("connecred");
+      console.log("connected");
     });
 
-    socket.on('changeColor', function (col) {
-      document.body.style.backgroundColor = col
-    });
+    // socket.on('changeColor', function (col) {
+    //   document.body.style.backgroundColor = col
+    // });
   }
 
   setColor(color){
@@ -42,15 +79,14 @@ class App extends Component{
 
     return(
       <div className="App">
-        <h1>{this.state.text}</h1>
+        <h1>{this.state.answer}</h1>
         <button onClick={this.onClick}>Send color to everyone</button>
-
         <button id="blue" onClick={() => this.setColor('blue')}>Blue</button>
         <button id="red" onClick={() => this.setColor('red')}>Red</button>
+        <LetterWrapper />
       </div>
     );
   }
-
 }
 
 export default hot(module)(App);
