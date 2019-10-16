2b2541bd10a40467ed0b341f96065966765b6135
Edward Greenberg <ed.w.greenberg@gmail.com>
2019-10-16 12:03:20 -0400

    add redirect to game from login

--- src/login.jsx
+++ src/login.jsx
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
 import logo from '../img_folder/team_Hang.jpg';
+import {Route, Redirect, Link} from 'react-router-dom';
 
 class Login extends Component {
     constructor(props) {
       super(props)
+      this.state = {
+          username : "",
+          password: "",
+          redirectToGame: false,
+      }
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event){
+            console.log('am I here in this universe')
+            this.setState({redirectToGame: true});
+            event.preventDefault();
+        
     }
   
     render() {
+
+        const redirectToGame = this.state.redirectToGame;
+        if (redirectToGame) return <Redirect to="/game" />
+
       return (
+          
           <React.Fragment>
               <div>
                   <img src={logo} alt="logo" className = "Logo" ></img>
-                  <form>
+                  <form onSubmit={this.handleSubmit}>
                       <label>
                           Username:
                           <input type = "text" name = "username"/>
