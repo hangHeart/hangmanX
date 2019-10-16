126eaf132237aff00eee51ef74a4a7f954d184f1
Andrew <ampaisner@gmail.com>
2019-10-16 19:45:25 -0400

    andrew

---
+++ server/test.js
@@ -0,0 +1,9 @@
+// const fs = require('fs');
+// const path = require('path');
+// // const wordCtrl = require('/wordController.js');
+
+// let x = fs.readFileSync(path.resolve(__dirname, 'American2.txt'));
+// x = `${x}`;
+// x = JSON.parse(x);
+// console.log(x)
+// wordCtrl.addWordAndClue(['hi', 'hi']);
